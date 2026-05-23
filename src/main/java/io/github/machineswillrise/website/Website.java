77a89474a16dc438ce97f83acf5c7b6bb35987ac
MachinesWillRise <machineswillrise@protonmail.com>
2026-05-23 11:12:02 -0400

    Initialize Maven archetype

---
+++ src/main/java/io/github/machineswillrise/website/Website.java
@@ -0,0 +1,7 @@
+package io.github.machineswillrise.website;
+
+public class Website {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/io/github/machineswillrise/website/WebsiteTest.java
@@ -0,0 +1,12 @@
+package io.github.machineswillrise.website;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class WebsiteTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
